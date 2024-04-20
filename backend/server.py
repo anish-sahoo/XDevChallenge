9e517f6658c909e706ce671a16bb782e0187c7d8
Anish Sahoo <anishsahoo2005@gmail.com>
2024-04-20 16:30:39 -0700

    Add server

---
+++ backend/server.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, jsonify, request
+from dotenv import load_dotenv
+from flask_cors import CORS, cross_origin
+
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello():
+    return 'Hello, World! This is my Flask application. This endpoint should not be called.'
+
+
+@app.route('/api/v1/search', methods=['POST'])
+@cross_origin()
+def test():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data.get('search_term'))
+        return jsonify({'message': f'{data.get("search_term", "No search term provided")}'})
+
+    return jsonify({'error': 'Method not allowed'}), 405
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv('PORT', 5000), host='127.0.0.1')
--- x-dev-challenge/src/App.tsx
+++ x-dev-challenge/src/App.tsx
@@ -1,18 +1,31 @@
 import SearchBar from "./components/searchBar.tsx";
 import { useState } from "react";
 import { HashLoader } from "react-spinners";
+import axios, {AxiosResponse, AxiosError} from "axios";
 
 function App() {
   const [searchClicked, setSearchClicked] = useState<boolean>(false);
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
-  const handleSearch = (term: string) => {
+  const getSearchResults = async (term: string) => {
+    // fetch data from the API
+    await axios.post("http://localhost:5000/api/v1/search", {
+      search_term: term,
+    }).then((response: AxiosResponse) => {
+      console.log(response.data);
+    }).catch((error: AxiosError) => {
+      console.log(error);
+    });
+  }
+
+  const handleSearch = async (term: string) => {
     setLoading(true);
     console.log(term);
     setTimeout(() => {
       setLoading(false);
     }, 4000);
+    await getSearchResults(term);
   };
 
   return (
@@ -30,15 +43,13 @@ function App() {
           searchClicked={searchClicked}
         />
         {loading && searchClicked && searchTerm.length > 0 && (
-          <div>
-            <HashLoader
-              cssOverride={{ display: "block", margin: "0 auto" }}
-              color="#ffffff"
-              loading={true}
-              size={50}
-            />
-          </div>
-        )}
+        <div
+          className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-opacity-50"
+          style={{ zIndex: 9999 }}
+        >
+          <HashLoader color="#ffffff" loading={true} size={50} />
+        </div>
+      )}
       </div>
     </>
   );
