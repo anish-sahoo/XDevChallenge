9a1f28780674417e30fe97a769e1ca72e9c8981b
Anish Sahoo <anishsahoo2005@gmail.com>
2024-04-20 12:44:50 -0700

    Install public-sans font

--- x-dev-challenge/src/App.tsx
+++ x-dev-challenge/src/App.tsx
@@ -1,12 +1,16 @@
 import SearchBar from "./components/searchBar.tsx";
 function App() {
-    return (
-        <>
-            <div className={'bg-black text-white font-mono flex h-screen w-screen items-center justify-center'}>
-                <SearchBar search={(term: string) => console.log(term) }/>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        className={
+          "bg-black text-white font-public-sans flex h-screen w-screen items-center justify-center"
+        }
+      >
+        <SearchBar search={(term: string) => console.log(term)} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
