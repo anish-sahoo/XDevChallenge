54ea340497d37bb3a5e599630abb47fa2f7899fb
William Riser <riser.w@northeastern.edu>
2024-04-20 13:47:53 -0700

    Add basic card styling

--- x-dev-challenge/src/App.tsx
+++ x-dev-challenge/src/App.tsx
@@ -1,16 +1,19 @@
 import SearchBar from "./components/searchBar.tsx";
+import Card from "./components/card.tsx";
+
 function App() {
-  return (
-    <>
-      <div
-        className={
-          "bg-black text-white font-public-sans flex h-screen w-screen items-center justify-center"
-        }
-      >
-        <SearchBar search={(term: string) => console.log(term)} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div
+                className={
+                    "bg-zinc-900 text-white font-public-sans flex flex-col min-h-screen w-screen items-center justify-center"
+                }
+            >
+                <SearchBar search={(term: string) => console.log(term)}/>
+                <Card/>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- x-dev-challenge/src/components/card.tsx
+++ x-dev-challenge/src/components/card.tsx
@@ -2,8 +2,13 @@
 
 const card = () => {
     return (
-        <div>
-            <h1>Card</h1>
+        <div className={"flex flex-col items-center rounded-md shadow-sm shadow-white bg-zinc-700 m-2 p-1"}>
+            <h1 className={"text-2xl text-white font-public-sans"}>
+                Card Name
+            </h1>
+            <p className={"text-sm text-white font-public-sans"}>
+                Card Description
+            </p>
         </div>
     )
 }
