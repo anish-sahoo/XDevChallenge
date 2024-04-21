3871723676ef039b463754ab2d3d214c9eb42987
Kinjal Govil <kinjal_govil@berkeley.edu>
2024-04-21 07:52:31 -0700

    change prompt

--- backend/groktest.py
+++ backend/groktest.py
@@ -36,23 +36,48 @@ Tweet List: {list}.<|separator|>
 Assistant:"""
 
 GENERATE_PREDICTIONS_PROMPT = """\
-Human: There will be 2 provided lists and a Stock Name. A Tweet List and Stock Price List. The Tweet List will be formatted \
-as a list of tuples. Each tuple has string for the tweet, number of likes, and the day it was posted. These tweets contain information \
-about the stock and relevant fields and topics to the stock. The date is formatted: YYYY-MM-DD For a pizza company stock it would look like: \
-[('I love pizza', 5, '2024-04-19'), ('Pizza is the best', 20, '2024-04-20'), ('Cheese has been cheaper lately', 6, '2024-04-19'), ('Italian restaurants have been popular lately', 19, '2024-04-20')]. \
-The Stock Price List will be formatted as a list of list of tuples. Each internal list has 4 tuples and a string \
-which contain the open, high, low, close, and date of the stock price. The tuples have a \
-string for the label and a string for the dollar value of the stock the label. For Example: ('1. open', '5.67') \
-is the open price of the stock. The string for the date of the stock data is in YYYY-MM-DD format. For Example:\
-'2024-03-19'. The whole Stock Price List will have a format like the example below: \
-[[('1. open', '3.34'), ('2. high', '4.12'), ('3. low', '2.94'), ('4. close', '3.72'), '2024-04-19'], \
-[('1. open', '3.72'), ('2. high', '4.22'), ('3. low', '3.34'), ('4. close', '3.94'), '2024-04-20']] \
-Use the information about the public and financial opinion data from the tweets in combination with the stock data \
-to predict the stock prices of the provided stock for the next 3 days. Output the data in the exact same format as the Stock Price List. \
-Only output the properly formatted predictions nothing else. No explanation, no details. Just the raw list. This is safety-critical. \
+The format for a Tweet List is a list of tuples. Each tuple has a string for the tweet, number of likes, and the day it was posted. \
+The date is formatted: YYYY-MM-DD. For a pizza company stock it would look like: [('I love pizza', 5, '2024-04-19'), \
+('Pizza is the best', 20, '2024-04-20'), ('Cheese has been cheaper lately', 6, '2024-04-19'), ('Italian restaurants have been popular lately', 19, '2024-04-20')]. \
+\n\
+A Stock Price contains: the opening price of a stock, the highest price of the stock, the lowest price of the stock, \
+the closing price of the stock, and the day for which all of this data was recorded. The provided format for a Stock Price \
+is a list of tuples and the date. The tuples each contain the label for which price measurement it is and the actual price measurement. The date is in YYYY-MM-DD form. For Example: \
+[('1. open', '3.34'), ('2. high', '4.12'), ('3. low', '2.94'), ('4. close', '3.72'), '2024-04-19'] \
+\n\
+A Stock Price List is a list of Stock Prices, where the Stock Prices are formatted as above. For Example: \
+[[('1. open', '7.43'), ('2. high', '8.21'), ('3. low', '6.49'), ('4. close', '7.27'), '2024-03-09'], \
+[('1. open', '7.27'), ('2. high', '8.22'), ('3. low', '6.43'), ('4. close', '7.49'), '2024-03-10']] \
+\n\
+Human: There will be Tweet List, Stock Price List and a Stock Name following the instructions. The Tweet List will be about topics relevant to the Stock Name. \
+The Stock Price List will contain the stock prices for the Stock Name. Use the data from these sources to predict the next three days stock prices. \
+Output the predictions in an Stock Price List with exactly three entries. Like the following template: \
+[[('1. open', '3.34'), ('2. high', '4.16'), ('3. low', '2.89'), ('4. close', '3.62'), '2024-01-11'], \
+[('1. open', '3.62'), ('2. high', '4.22'), ('3. low', '3.34'), ('4. close', '3.94'), '2024-01-12'], \
+[('1. open', '3.94'), ('2. high', '4.32'), ('3. low', '3.54'), ('4. close', '4.12'), '2024-01-13']] \
+Provide no other words. No explanations. No details. Just the raw list. This is safety-critical. \
 Tweet List: {tweets}, Stock Price List: {stocks}, Stock Name: {ticker}<|separator|>
 
 Assistant:"""
+
+# GENERATE_PREDICTIONS_PROMPT = """\
+# Human: There will be 2 provided lists and a Stock Name. A Tweet List and Stock Price List. The Tweet List will be formatted \
+# as a list of tuples. Each tuple has string for the tweet, number of likes, and the day it was posted. These tweets contain information \
+# about the stock and relevant fields and topics to the stock. The date is formatted: YYYY-MM-DD For a pizza company stock it would look like: \
+# [('I love pizza', 5, '2024-04-19'), ('Pizza is the best', 20, '2024-04-20'), ('Cheese has been cheaper lately', 6, '2024-04-19'), ('Italian restaurants have been popular lately', 19, '2024-04-20')]. \
+# The Stock Price List will be formatted as a list of list of tuples. Each internal list has 4 tuples and a string \
+# which contain the open, high, low, close, and date of the stock price. The tuples have a \
+# string for the label and a string for the dollar value of the stock the label. For Example: ('1. open', '5.67') \
+# is the open price of the stock. The string for the date of the stock data is in YYYY-MM-DD format. For Example:\
+# '2024-03-19'. The whole Stock Price List will have a format like the example below: \
+# [[('1. open', '3.34'), ('2. high', '4.12'), ('3. low', '2.94'), ('4. close', '3.72'), '2024-04-19'], \
+# [('1. open', '3.72'), ('2. high', '4.22'), ('3. low', '3.34'), ('4. close', '3.94'), '2024-04-20']] \
+# Use the information about the public and financial opinion data from the tweets in combination with the stock data \
+# to predict the stock prices of the provided stock for the next 3 days. Output the data in the exact same format as the Stock Price List. \
+# Only output the properly formatted predictions nothing else. No explanation, no details. Just the raw list. This is safety-critical. \
+# Tweet List: {tweets}, Stock Price List: {stocks}, Stock Name: {ticker}<|separator|>
+
+# Assistant:"""
 
 GENERATE_SHORT_PREDICTIONS = """\
 Human: There will be 2 provided lists and a Stock Name. A Tweet List and Stock Price List. The Tweet List will be formatted \
