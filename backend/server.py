724d4c29501ae0532658fdd03ebd4f30eeda3da1
Anish Sahoo <anishsahoo2005@gmail.com>
2024-04-21 09:50:02 -0700

    new stuff

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ import os
 from quart import Quart, jsonify, request
 from dotenv import load_dotenv
 from quart_cors import cors
-from groktest import get_terms, gen_predictions, gen_short, gen_long
+from groktest import get_terms, gen_predictions, gen_short, gen_long, gen_old_predictions
 from searchAllTest import get_all_tweets
 from stockdata import getStockData
 import json
@@ -67,6 +67,8 @@ async def predict():
             print('Gen Long>>>>>>>', prediction2)
             prediction3 = await gen_predictions(tweets_list, stock_data, stock_name)
             print('Gen Predictions>>>>>>>', prediction3)
+            prediction4 = await gen_old_predictions(tweets_list, stock_data, stock_name)
+            print('Gen Old Predictions>>>>>>>', prediction4)
             return prediction
         except Exception as e:
             print('Exception',e)
